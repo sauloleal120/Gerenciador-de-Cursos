4fe56f8b67d9505adcea421d56327451aab4e7ae
Saulo <82407162+sauloleal120@users.noreply.github.com>
2024-04-22 17:27:17 -0300

    construindo tela Courses

--- src/screens/CoursesScreen.tsx
+++ src/screens/CoursesScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, Image, StyleSheet, Switch, FlatList, TouchableOpacity, SafeAreaView  } from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput  } from 'react-native';
 
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -9,16 +9,26 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export function CoursesScreen() {
 
-   
-
-  
 
     return (
 
-      
-<View style={styles.mainContainer}>
-    <Text> Courses Screen</Text>
-</View>
+      <View style={styles.mainContainer}>
+
+              <Text style={styles.greeting}> Hello, </Text>
+
+              <View style={styles.nameContainer}>
+                 <Text style={styles.name}> Saulo Leal </Text>
+                 <MaterialCommunityIcons style={styles.bell} name="bell-ring-outline" size={40} color="black" />
+              </View>
+          
+          
+
+          <TextInput placeholder='Search course' style={styles.input} />
+         
+      </View>
+
+            
+
   
 
     );}
@@ -29,13 +39,45 @@ export function CoursesScreen() {
 
   const styles = StyleSheet.create({
 
-    mainContainer:{ 
-      flex: 1, 
-      alignItems: 'center', 
-      justifyContent:'center'
-       
-  
+    mainContainer:{
+        margin:20
     },
+
+    greeting:{
+        marginLeft:10,
+        marginTop: 20
+    },
+
+
+
+    nameContainer:{
+
+        flexDirection:'row',
+        
+    },
+
+    name:{
+        fontSize: 50,
+        fontWeight:'bold',
+        textAlign:'left',
+
+    },
+
+    bell:{
+        marginTop: 15,
+        marginLeft:30
+    },
+
+    input:{
+        borderColor:'black',
+        borderWidth:1,
+        marginTop:10,
+        borderRadius: 15,
+        height: 60,
+        padding: 20,
+        fontSize:17
+    },
+
     
     
 
